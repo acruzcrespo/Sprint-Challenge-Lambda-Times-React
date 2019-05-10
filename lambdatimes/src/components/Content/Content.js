3c17d20e85f6533d5b38e3cf05c0f7f2a29f8f3f
Ash <ashley.cruz.crespo@gmail.com>
2019-05-10 12:03:47 -0400

    fixing bugs

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-
+import styled from 'styled-components';
 import Tabs from './Tabs';
 import Cards from './Cards';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
+
+const ContentContainer = styled.div`
+  display: flex; 
+  flex-direction: column; 
+  align-items: center;
+`;
 
 export default class Content extends Component {
   constructor(props) {
@@ -17,10 +23,18 @@ export default class Content extends Component {
   }
 
   componentDidMount() {
+    this.setState({ 
+      tabs: tabData,
+      cards: cardData
+     })
     // Once the component has mounted, get the data and reflect that data on the state.
   }
 
   changeSelected = tab => {
+    changeSelected = tab => {
+      this.setState({ 
+        selected: tab
+       })
     // this function should take in the tab and update the state with the new tab.
   };
 
@@ -37,20 +51,24 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if (this.state.selected === 'all'){
+      return this.state.cards;
+    } else {
+      return this.state.cards.filter( card => card.tab === this.state.selected)
+    }
   };
-
+}
   render() {
     return (
-      <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
-        */}
-        <Tabs tabs={this.state.tabs} />
+      <ContentContainer>
+        
+        <Tabs
+        tabs={this.state.tabs}
+        selectedTab={this.changeSelected}
+        selectTabHandler={this.filterCards}
+        />
         <Cards cards={this.filterCards()} />
-      </div>
-    );
-  }
+      </ContentContainer>
+      );
+    }
 }
